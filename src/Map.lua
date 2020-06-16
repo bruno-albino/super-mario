7a6ea920a97d2e49c584cd7bf803d06dbda148c8
Bruno Albino <bruno-albino@hotmail.com>
2020-06-16 19:28:35 -0300

    Collision update and block animation

--- src/Map.lua
+++ src/Map.lua
@@ -1,3 +1,8 @@
+--[[
+    Contains tile data and necessary code for rendering a tile map to the
+    screen.
+]]
+
 require 'Util'
 require 'Player'
 
@@ -6,164 +11,188 @@ Map = Class{}
 TILE_BRICK = 1
 TILE_EMPTY = -1
 
--- CLOUD TILES
+-- cloud tiles
 CLOUD_LEFT = 6
 CLOUD_RIGHT = 7
 
--- BUSH TILES
+-- bush tiles
 BUSH_LEFT = 2
 BUSH_RIGHT = 3
 
--- MUSHROOM TILES
+-- mushroom tiles
 MUSHROOM_TOP = 10
-MUSHROOM_BOTTTOM = 11
+MUSHROOM_BOTTOM = 11
 
--- BLOCK TILE
+-- jump block
 JUMP_BLOCK = 5
+JUMP_BLOCK_HIT = 9
 
-
+-- a speed to multiply delta time to scroll map; smooth value
 local SCROLL_SPEED = 62
 
+-- constructor for our map object
 function Map:init()
 
-  self.spritesheet = love.graphics.newImage('graphics/spritesheet.png') 
-  
+    self.spritesheet = love.graphics.newImage('graphics/spritesheet.png')
+    self.sprites = generateQuads(self.spritesheet, 16, 16)
 
-  self.tileWidth = 16
-  self.tileHeight = 16
-  self.mapWidth = 30
-  self.mapHeight = 28
-  self.tiles = {}
+    self.tileWidth = 16
+    self.tileHeight = 16
+    self.mapWidth = 30
+    self.mapHeight = 28
+    self.tiles = {}
 
-  self.player = Player(self)
+    -- applies positive Y influence on anything affected
+    self.gravity = 15
 
-  -- camera offsets
-  self.camX = 0
-  self.camY = 0
+    -- associate player with map
+    self.player = Player(self)
 
-  self.sprites = generateQuads(self.spritesheet, self.tileWidth, self.tileHeight)
+    -- camera offsets
+    self.camX = 0
+    self.camY = -3
 
-  -- cache width and height of map in pixels
-  self.mapWidthPixels = self.mapWidth * self.tileWidth
-  self.mapHeightPixels = self.mapHeight * self.tileHeight
+    -- cache width and height of map in pixels
+    self.mapWidthPixels = self.mapWidth * self.tileWidth
+    self.mapHeightPixels = self.mapHeight * self.tileHeight
 
-  -- first, fill map with empty tiles
-  for y = 1, self.mapHeight do
-    for x = 1, self.mapWidth do
-
-      self:setTile(x, y, TILE_EMPTY)
-    end
-  end
-
-  -- begin generating the terrain using vertical scan lines
-  local x = 1
-  while x < self.mapWidth do
-
-    -- 2% change to generate a cloud
-    -- make sure we're 2 tiles from edge at least
-    if x < self.mapWidth - 2 then
-      if math.random(20) == 1 then
-
-        -- choose a random vertical spot above where blocks/pipes generate
-        local cloudStart = math.random(self.mapHeight / 2 - 6)
-
-        self:setTile(x, cloudStart, CLOUD_LEFT)
-        self:setTile(x + 1, cloudStart, CLOUD_RIGHT)
-      end
+    -- first, fill map with empty tiles
+    for y = 1, self.mapHeight do
+        for x = 1, self.mapWidth do
+            
+            -- support for multiple sheets per tile; storing tiles as tables 
+            self:setTile(x, y, TILE_EMPTY)
+        end
     end
 
-    -- 5% CHANGE TO GENERATE A MUSHROOM
-    if math.random(20) == 1 then
+    -- begin generating the terrain using vertical scan lines
+    local x = 1
+    while x < self.mapWidth do
+        
+        -- 2% chance to generate a cloud
+        -- make sure we're 2 tiles from edge at least
+        if x < self.mapWidth - 2 then
+            if math.random(20) == 1 then
+                
+                -- choose a random vertical spot above where blocks/pipes generate
+                local cloudStart = math.random(self.mapHeight / 2 - 6)
 
-      -- left side of pipe
-      self:setTile(x, self.mapHeight / 2 - 2, MUSHROOM_TOP)
-      self:setTile(x, self.mapHeight / 2 - 1, MUSHROOM_BOTTTOM)
+                self:setTile(x, cloudStart, CLOUD_LEFT)
+                self:setTile(x + 1, cloudStart, CLOUD_RIGHT)
+            end
+        end
 
-      -- CREATES COLUMN OF TILES GOING TO BOTTOM OF MAP
-      for y = self.mapHeight / 2, self.mapHeight do
-        self:setTile(x, y, TILE_BRICK)
-      end
+        -- 5% chance to generate a mushroom
+        if math.random(20) == 1 then
+            -- left side of pipe
+            self:setTile(x, self.mapHeight / 2 - 2, MUSHROOM_TOP)
+            self:setTile(x, self.mapHeight / 2 - 1, MUSHROOM_BOTTOM)
 
-      -- next vertical scan line
-      x = x + 1
+            -- creates column of tiles going to bottom of map
+            for y = self.mapHeight / 2, self.mapHeight do
+                self:setTile(x, y, TILE_BRICK)
+            end
 
-    -- 10% change to generate bush, being sure to generate away from edge
-    elseif math.random(10) == 1 and x < self.mapWidth - 3 then
-      local bushLevel = self.mapHeight / 2 - 1
+            -- next vertical scan line
+            x = x + 1
 
-      -- place bush component and then column  of bricks
-      self:setTile(x, bushLevel, BUSH_LEFT)
-      for y = self.mapHeight / 2, self.mapHeight do
-        self:setTile(x, y, TILE_BRICK)
-      end
+        -- 10% chance to generate bush, being sure to generate away from edge
+        elseif math.random(10) == 1 and x < self.mapWidth - 3 then
+            local bushLevel = self.mapHeight / 2 - 1
 
-      x = x + 1
+            -- place bush component and then column of bricks
+            self:setTile(x, bushLevel, BUSH_LEFT)
+            for y = self.mapHeight / 2, self.mapHeight do
+                self:setTile(x, y, TILE_BRICK)
+            end
+            x = x + 1
 
-      self:setTile(x, bushLevel, BUSH_RIGHT)
-      for y = self.mapHeight / 2, self.mapHeight do
-        self:setTile(x, y, TILE_BRICK)
-      end
+            self:setTile(x, bushLevel, BUSH_RIGHT)
+            for y = self.mapHeight / 2, self.mapHeight do
+                self:setTile(x, y, TILE_BRICK)
+            end
+            x = x + 1
 
-    -- 10% change to not generate anything , creating a gap
-    elseif math.random(10) ~= 1 then
+        -- 10% chance to not generate anything, creating a gap
+        elseif math.random(10) ~= 1 then
+            
+            -- creates column of tiles going to bottom of map
+            for y = self.mapHeight / 2, self.mapHeight do
+                self:setTile(x, y, TILE_BRICK)
+            end
 
-      -- create column of tiles going to bottom of map
-      for y = self.mapHeight / 2, self.mapHeight do
-        self:setTile(x, y, TILE_BRICK)
-      end
+            -- chance to create a block for Mario to hit
+            if math.random(15) == 1 then
+                self:setTile(x, self.mapHeight / 2 - 4, JUMP_BLOCK)
+            end
 
-      -- change to create a block for mario to hit
-      if math.random(15) == 1 then
-        self:setTile(x, self.mapHeight / 2 - 4, JUMP_BLOCK)
-      end
-
-      x = x + 1
-    else
-      -- increment x so we skip two scanlines, creating 2-tile gap
-      x = x + 2
+            -- next vertical scan line
+            x = x + 1
+        else
+            -- increment X so we skip two scanlines, creating a 2-tile gap
+            x = x + 2
+        end
     end
-
-  end
-  
-
-
-
-  -- starts halway down the map, populates with bricks
-  for y = self.mapHeight / 2, self.mapHeight do
-    
-    for x = 1, self.mapWidth do
-      self:setTile(x, y, TILE_BRICK)
-    end
-
-  end
 end
 
+-- return whether a given tile is collidable
+function Map:collides(tile)
+    -- define our collidable tiles
+    local collidables = {
+        TILE_BRICK, JUMP_BLOCK, JUMP_BLOCK_HIT,
+        MUSHROOM_TOP, MUSHROOM_BOTTOM
+    }
+
+    -- iterate and return true if our tile type matches
+    for _, v in ipairs(collidables) do
+        if tile.id == v then
+            return true
+        end
+    end
+
+    return false
+end
+
+-- function to update camera offset with delta time
 function Map:update(dt)
-  self.camX = math.max(0, math.min(self.player.x - VIRTUAL_WIDTH / 2, math.min(self.mapWidthPixels - VIRTUAL_WIDTH, self.player.x) ))
-
-  self.player:update(dt)
+    self.player:update(dt)
+    
+    -- keep camera's X coordinate following the player, preventing camera from
+    -- scrolling past 0 to the left and the map's width
+    self.camX = math.max(0, math.min(self.player.x - VIRTUAL_WIDTH / 2,
+        math.min(self.mapWidthPixels - VIRTUAL_WIDTH, self.player.x)))
 end
 
-function Map:setTile(x, y, tile)
-
-  self.tiles[(y - 1) * self.mapWidth + x] = tile
-
+-- gets the tile type at a given pixel coordinate
+function Map:tileAt(x, y)
+    return {
+        x = math.floor(x / self.tileWidth) + 1,
+        y = math.floor(y / self.tileHeight) + 1,
+        id = self:getTile(math.floor(x / self.tileWidth) + 1, math.floor(y / self.tileHeight) + 1)
+    }
 end
 
+-- returns an integer value for the tile at a given x-y coordinate
 function Map:getTile(x, y)
-  return self.tiles[(y - 1) * self.mapWidth + x]
+    return self.tiles[(y - 1) * self.mapWidth + x]
 end
 
+-- sets a tile at a given x-y coordinate to an integer value
+function Map:setTile(x, y, id)
+    self.tiles[(y - 1) * self.mapWidth + x] = id
+end
+
+-- renders our map to the screen, to be called by main's render
 function Map:render()
-  for y = 1, self.mapHeight do
-
-    for x = 1, self.mapWidth do
-      local tile = self:getTile(x, y)
-      if tile ~= TILE_EMPTY then
-        love.graphics.draw(self.spritesheet, self.sprites[tile], (x - 1) * self.tileWidth, (y - 1) * self.tileHeight)
-      end
+    for y = 1, self.mapHeight do
+        for x = 1, self.mapWidth do
+            local tile = self:getTile(x, y)
+            if tile ~= TILE_EMPTY then
+                love.graphics.draw(self.spritesheet, self.sprites[tile],
+                    (x - 1) * self.tileWidth, (y - 1) * self.tileHeight)
+            end
+        end
     end
-  end
 
-  self.player:render()
+    self.player:render()
 end
--- src/Player.lua
+++ src/Player.lua
@@ -1,141 +1,257 @@
-
-require 'Animation'
+--[[
+    Represents our player in the game, with its own sprite.
+]]
 
 Player = Class{}
 
-local MOVE_SPEED = 80
+require 'Animation'
+
+local WALKING_SPEED = 140
 local JUMP_VELOCITY = 400
-local GRAVITY = 40
 
 function Player:init(map)
+    
+    self.x = 0
+    self.y = 0
     self.width = 16
     self.height = 20
 
-    self.dy = 0
-    self.dx = 0
+    -- offset from top left to center to support sprite flipping
+    self.xOffset = 8
+    self.yOffset = 10
 
-    self.x = map.tileWidth * 10
-    self.y = map.tileHeight * (map.mapHeight / 2 - 1) - self.height
-
+    -- reference to map for checking tiles
+    self.map = map
     self.texture = love.graphics.newImage('graphics/blue_alien.png')
-    self.frames = generateQuads(self.texture, 16, 20)
-    
+
+    -- animation frames
+    self.frames = {}
+
+    -- current animation frame
+    self.currentFrame = nil
+
+    -- used to determine behavior and animations
     self.state = 'idle'
-    self.direction = 'right'
-    
+
+    -- determines sprite flipping
+    self.direction = 'left'
+
+    -- x and y velocity
+    self.dx = 0
+    self.dy = 0
+
+    -- position on top of map tiles
+    self.y = map.tileHeight * ((map.mapHeight - 2) / 2) - self.height
+    self.x = map.tileWidth * 10
+
+    -- initialize all player animations
     self.animations = {
-        ['idle'] = Animation {
+        ['idle'] = Animation({
             texture = self.texture,
             frames = {
-                self.frames[1]
-            },
-            interval = 1
-        },
-        ['walking'] = Animation {
+                love.graphics.newQuad(0, 0, 16, 20, self.texture:getDimensions())
+            }
+        }),
+        ['walking'] = Animation({
             texture = self.texture,
             frames = {
-                self.frames[9],
-                self.frames[10],
-                self.frames[11]
+                love.graphics.newQuad(128, 0, 16, 20, self.texture:getDimensions()),
+                love.graphics.newQuad(144, 0, 16, 20, self.texture:getDimensions()),
+                love.graphics.newQuad(160, 0, 16, 20, self.texture:getDimensions()),
+                love.graphics.newQuad(144, 0, 16, 20, self.texture:getDimensions()),
             },
             interval = 0.15
-        },
-        ['jumping'] = Animation {
+        }),
+        ['jumping'] = Animation({
             texture = self.texture,
             frames = {
-                self.frames[3]
-            },
-            interval = 1
-        }
+                love.graphics.newQuad(32, 0, 16, 20, self.texture:getDimensions())
+            }
+        })
     }
 
+    -- initialize animation and current frame we should render
     self.animation = self.animations['idle']
+    self.currentFrame = self.animation:getCurrentFrame()
 
+    -- behavior map we can call based on player state
     self.behaviors = {
         ['idle'] = function(dt)
+            
+            -- add spacebar functionality to trigger jump state
             if love.keyboard.wasPressed('space') then
                 self.dy = -JUMP_VELOCITY
                 self.state = 'jumping'
                 self.animation = self.animations['jumping']
-
             elseif love.keyboard.isDown('a') then
-                self.dx = -MOVE_SPEED
-
-                self.animation = self.animations['walking']
                 self.direction = 'left'
-
-            elseif love.keyboard.isDown('d') then
-                self.dx = MOVE_SPEED
-
+                self.dx = -WALKING_SPEED
+                self.state = 'walking'
+                self.animations['walking']:restart()
                 self.animation = self.animations['walking']
-
+            elseif love.keyboard.isDown('d') then
                 self.direction = 'right'
+                self.dx = WALKING_SPEED
+                self.state = 'walking'
+                self.animations['walking']:restart()
+                self.animation = self.animations['walking']
             else
-                self.animation = self.animations['idle']
+                self.dx = 0
             end
         end,
         ['walking'] = function(dt)
+            
+            -- keep track of input to switch movement while walking, or reset
+            -- to idle if we're not moving
             if love.keyboard.wasPressed('space') then
                 self.dy = -JUMP_VELOCITY
                 self.state = 'jumping'
                 self.animation = self.animations['jumping']
             elseif love.keyboard.isDown('a') then
-                self.dx = -MOVE_SPEED
-                self.animation = self.animations['walking']
                 self.direction = 'left'
-
+                self.dx = -WALKING_SPEED
             elseif love.keyboard.isDown('d') then
-                self.dx = MOVE_SPEED
-                self.animation = self.animations['walking']
                 self.direction = 'right'
-
+                self.dx = WALKING_SPEED
             else
+                self.dx = 0
+                self.state = 'idle'
                 self.animation = self.animations['idle']
+            end
+
+            -- check for collisions moving left and right
+            self:checkRightCollision()
+            self:checkLeftCollision()
+
+            -- check if there's a tile directly beneath us
+            if not self.map:collides(self.map:tileAt(self.x, self.y + self.height)) and
+                not self.map:collides(self.map:tileAt(self.x + self.width - 1, self.y + self.height)) then
                 
+                -- if so, reset velocity and position and change state
+                self.state = 'jumping'
+                self.animation = self.animations['jumping']
             end
         end,
         ['jumping'] = function(dt)
+            -- break if we go below the surface
+            if self.y > 300 then
+                return
+            end
+
             if love.keyboard.isDown('a') then
                 self.direction = 'left'
-                self.dx = -MOVE_SPEED
-
+                self.dx = -WALKING_SPEED
             elseif love.keyboard.isDown('d') then
                 self.direction = 'right'
-                self.dx = MOVE_SPEED
+                self.dx = WALKING_SPEED
             end
 
-            self.dy = self.dy + GRAVITY
+            -- apply map's gravity before y velocity
+            self.dy = self.dy + self.map.gravity
 
-            if self.y >= map.tileHeight * (map.mapHeight / 2 - 1) - self.height then
-                self.y = map.tileHeight * (map.mapHeight / 2 - 1) - self.height
+            -- check if there's a tile directly beneath us
+            if self.map:collides(self.map:tileAt(self.x, self.y + self.height)) or
+                self.map:collides(self.map:tileAt(self.x + self.width - 1, self.y + self.height)) then
+                
+                -- if so, reset velocity and position and change state
                 self.dy = 0
                 self.state = 'idle'
-                self.animation = self.animations[self.state]
+                self.animation = self.animations['idle']
+                self.y = (self.map:tileAt(self.x, self.y + self.height).y - 1) * self.map.tileHeight - self.height
             end
 
+            -- check for collisions moving left and right
+            self:checkRightCollision()
+            self:checkLeftCollision()
         end
     }
 end
 
 function Player:update(dt)
     self.behaviors[self.state](dt)
-
     self.animation:update(dt)
-
+    self.currentFrame = self.animation:getCurrentFrame()
     self.x = self.x + self.dx * dt
-    self.y = self.y + self.dy * dt
 
+    self:calculateJumps()
+
+    -- apply velocity
+    self.y = self.y + self.dy * dt
+end
+
+-- jumping and block hitting logic
+function Player:calculateJumps()
+    
+    -- if we have negative y velocity (jumping), check if we collide
+    -- with any blocks above us
+    if self.dy < 0 then
+        if self.map:tileAt(self.x, self.y).id ~= TILE_EMPTY or
+            self.map:tileAt(self.x + self.width - 1, self.y).id ~= TILE_EMPTY then
+            -- reset y velocity
+            self.dy = 0
+
+            -- change block to different block
+            local playCoin = false
+            local playHit = false
+            if self.map:tileAt(self.x, self.y).id == JUMP_BLOCK then
+                self.map:setTile(math.floor(self.x / self.map.tileWidth) + 1,
+                    math.floor(self.y / self.map.tileHeight) + 1, JUMP_BLOCK_HIT)
+                playCoin = true
+            else
+                playHit = true
+            end
+            if self.map:tileAt(self.x + self.width - 1, self.y).id == JUMP_BLOCK then
+                self.map:setTile(math.floor((self.x + self.width - 1) / self.map.tileWidth) + 1,
+                    math.floor(self.y / self.map.tileHeight) + 1, JUMP_BLOCK_HIT)
+                playCoin = true
+            else
+                playHit = true
+            end
+
+        end
+    end
+end
+
+-- checks two tiles to our left to see if a collision occurred
+function Player:checkLeftCollision()
+    if self.dx < 0 then
+        -- check if there's a tile directly beneath us
+        if self.map:collides(self.map:tileAt(self.x - 1, self.y)) or
+            self.map:collides(self.map:tileAt(self.x - 1, self.y + self.height - 1)) then
+            
+            -- if so, reset velocity and position and change state
+            self.dx = 0
+            self.x = self.map:tileAt(self.x - 1, self.y).x * self.map.tileWidth
+        end
+    end
+end
+
+-- checks two tiles to our right to see if a collision occurred
+function Player:checkRightCollision()
+    if self.dx > 0 then
+        -- check if there's a tile directly beneath us
+        if self.map:collides(self.map:tileAt(self.x + self.width, self.y)) or
+            self.map:collides(self.map:tileAt(self.x + self.width, self.y + self.height - 1)) then
+            
+            -- if so, reset velocity and position and change state
+            self.dx = 0
+            self.x = (self.map:tileAt(self.x + self.width, self.y).x - 1) * self.map.tileWidth - self.width
+        end
+    end
 end
 
 function Player:render()
-
     local scaleX
+
+    -- set negative x scale factor if facing left, which will flip the sprite
+    -- when applied
     if self.direction == 'right' then
         scaleX = 1
     else
         scaleX = -1
     end
 
-
-    love.graphics.draw(self.texture, self.animation:getCurrentFrame(), math.floor(self.x + self.width / 2), math.floor(self.y + self.height / 2), 0, scaleX, 1, self.width / 2, self.height / 2)
+    -- draw sprite with scale factor and offsets
+    love.graphics.draw(self.texture, self.currentFrame, math.floor(self.x + self.xOffset),
+        math.floor(self.y + self.yOffset), 0, scaleX, 1, self.xOffset, self.yOffset)
 end
